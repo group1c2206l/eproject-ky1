803c9aeea1363bacd095eed1793b55b353d2a4ef
duanhoang <duanhoang1995@gmail.com>
2023-03-04 23:15:23 +0700

    duan

--- course.php
+++ course.php
@@ -40,8 +40,8 @@
                     </div>
                     <div class="row-content">
                         <div class="tittle">
-                            <h1>Matial arts</h1>
-                            <p>
+                            <h1 font-size: 28px; >Matial arts</h1>
+                            <p style="font-size:16px;margin-bottom:20px; ">
                                 Võ thuật (martial arts) là một hình thức nghệ thuật, thể thao và tự vệ<br>
                                 được phát triển từ các kỹ năng và chiến thuật chiến đấu. Võ thuật bao gồm<br>
                                 nhiều phương pháp và hình thức khác nhau, từ các bộ môn truyền thống của<br>
@@ -51,8 +51,8 @@
                         </div>
                         <div class="learn-about" >
                             <a href=""><img src="./assets/image/anh/btn-orange-1.png" alt=""></a>
-                            <div style="position: absolute; top: 40%; left: 20%; transform: translate(-50%, -50%); z-index: 1;">
-                            <a href=""><p>Tìm hiểu thêm</p></a>
+                            <div style="position: absolute; top: 40%; left: 17%; transform: translate(-50%, -50%); z-index: 1;">
+                            <a href=""><p style="font-size:16px">Đăng ký ngay</p></a>
                             </div>
                         </div>
                     </div>
@@ -60,8 +60,8 @@
                 <div class="row2" style=" background-image: url(./assets/image/anh/c2.jpg); background-size: cover;height: 620px;position: relative;">
                     <div class="row-content">
                         <div class="tittle">
-                            <h1>Gym & fitness</h1>
-                            <p>
+                            <h1 font-size: 28px;>Gym & fitness</h1>
+                            <p style="font-size:16px;margin-bottom:20px;">
                                 Phòng tập gym và tập luyện fitness là nơi mà người tập thể dục và rèn luyện<br>
                                 sức khỏe bằng cách sử dụng các thiết bị tập luyện và các bài tập thể dục. Tập<br>
                                 luyện thể dục và rèn luyện sức khỏe là một phần quan trọng của một lối sống<br>
@@ -71,8 +71,8 @@
                         </div>
                         <div class="learn-about">
                             <a href=""><img src="./assets/image/anh/btn-orange-1.png" alt=""></a>
-                            <div style="position: absolute; top: 40%; left: 20%; transform: translate(-50%, -50%); z-index: 1;">
-                            <a href=""><p>Tìm hiểu thêm</p></a>
+                            <div style="position: absolute; top: 40%; left: 17%; transform: translate(-50%, -50%); z-index: 1;">
+                            <a href=""><p style="font-size:16px" >Đăng ký ngay</p></a>
                             </div>
                         </div>
                     </div>
@@ -80,8 +80,8 @@
                 <div class="row3" style=" background-image: url(./assets/image/anh/dancee.jpg); background-size: cover;height: 620px;position: relative;">
                     <div class="row-content">
                         <div class="tittle">
-                            <h1>Dance</h1>
-                            <p>
+                            <h1 font-size: 28px;>Dance</h1>
+                            <p style="font-size:16px;margin-bottom:20px;">
                                 Dance là một nghệ thuật biểu diễn mà nó thể hiện qua những bước nhảy phối</br>
                                 hợp với âm nhạc. Nó có thể được thực hiện một mình hoặc nhóm, trong phòng tập,</br>
                                 trên sân khấu hoặc trên đường phố. Dance đã trở thành một phần của nhiều nền</br>
@@ -91,26 +91,30 @@
                         </div>
                         <div class="learn-about">
                             <a href=""><img src="./assets/image/anh/btn-orange-1.png" alt=""></a>
-                            <div style="position: absolute; top: 40%; left: 20%; transform: translate(-50%, -50%); z-index: 1;">
-                            <a href=""><p>Tìm hiểu thêm</p></a>
+                            <div style="position: absolute; top: 40%; left: 17%; transform: translate(-50%, -50%); z-index: 1;">
+                            <a href=""><p style="font-size:16px">Đăng ký ngay</p></a>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="row3" style=" background-image: url(./assets/image/anh/zumm_auto_x2.jpg); background-size: cover;height: 620px;position: relative;">
+                <div class="row4" style=" background-image: url(./assets/image/anh/zumm_auto_x2.jpg); background-size: cover;height: 620px;position: relative;">
                    <div class="row-content">
                     <div class="tittle">
-                        <h1>Zumba</h1>
-                        <p>
+                        <h1 style="font-size: 28px;" >Zumba</h1>
+                        <p style="font-size:16px;margin-bottom:20px;">
                             Zumba là một loại hình thể dục thể thao kết hợp giữa các bài tập nhảy và nhạc latin.<br>
                             Nó đã trở thành một trào lưu tập thể dục phổ biến trên toàn thế giới với hàng triệu người<br>
                             tham gia mỗi ngày.<br>
+                            Zumba bao gồm các bài tập aerobic và nhảy, kết hợp với nhạc sôi động từ các thể loại âm <br>
+                            nhạc khác nhau như salsa, merengue, cumbia, reggaeton, samba, hip-hop và nhiều thể loại <br>
+                            khác. Với Zumba, người tập có thể tập trung vào việc di chuyển và nhảy theo nhạc, giúp họ <br>
+                            cảm thấy vui vẻ và thư giãn.
                         </p>
                    </div>
                     <div class="learn-about">
                         <a href=""><img src="./assets/image/anh/btn-orange-1.png" alt=""></a>
-                        <div style="position: absolute; top: 40%; left: 20%; transform: translate(-50%, -50%); z-index: 1;">
-                        <a href=""><p>Tìm hiểu thêm</p></a>
+                        <div style="position: absolute; top: 40%; left: 17%; transform: translate(-50%, -50%); z-index: 1;">
+                        <a href=""><p style="font-size:16px">Đăng ký ngay</p></a>
                         </div>
                     </div>
                    </div>
